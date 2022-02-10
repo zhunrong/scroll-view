dc01f4eaa1db96e2707749583cf7f5489b9780bb
zhunrong <chenzr@chd.edu.cn>
2022-02-10 18:49:07 +0800

    eslint (#5)

--- dev/main.ts
+++ dev/main.ts
@@ -2,9 +2,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import plugin from '@package';
 
-console.dir(plugin);
 Vue.use(plugin);
 
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- package/index.ts
+++ package/index.ts
@@ -1,14 +1,14 @@
 import component from './scroll-view';
-import { PluginObject } from "vue";
+import { PluginObject } from 'vue';
 
 type ScrollView = typeof component & PluginObject<void>;
 
 const plugin = component as ScrollView;
 
 plugin.install = function (Vue) {
-  Vue.component("scroll-view", component);
+  Vue.component('ScrollView', component);
 };
 
-plugin.version = process.env.VERSION!;
+plugin.version = process.env.VERSION;
 
 export default plugin;
